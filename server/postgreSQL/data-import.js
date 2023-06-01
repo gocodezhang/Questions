0b995d6e3cf5ae1c98332a038fb5ef7004a04130
Gaoyuan Zhang <zgy25483387@gmail.com>
2023-06-01 08:53:30 -0700

    update server

--- server/postgreSQL/data-import.js
+++ server/postgreSQL/data-import.js
@@ -4,8 +4,6 @@ require('dotenv').config();
 
 const client = new Client({
   database: 'questionapi',
-  user: 'jayzhang',
-  password: process.env.PASSWORD,
 });
 
 client.connect();
@@ -14,7 +12,7 @@ const questionsPath = path.join(__dirname, '../../questions-data/questions.csv')
 const table1 = 'landing_questions';
 
 client
-  .query(`\copy ${table1} FROM '${questionsPath}' WITH (FORMAT CSV, HEADER)`)
+  .query(`COPY ${table1} FROM '${questionsPath}' WITH (FORMAT CSV, HEADER)`)
   .then(() => {
     console.log('Successfully imported: ', table1);
   })
@@ -24,7 +22,7 @@ const answersPath = path.join(__dirname, '../../questions-data/answers.csv');
 const table2 = 'landing_answers';
 
 client
-  .query(`\copy ${table2} FROM '${answersPath}' WITH (FORMAT CSV, HEADER)`)
+  .query(`COPY ${table2} FROM '${answersPath}' WITH (FORMAT CSV, HEADER)`)
   .then(() => {
     console.log('Successfully imported: ', table2);
   })
@@ -34,7 +32,7 @@ const photosPath = path.join(__dirname, '../../questions-data/answers_photos.csv
 const table3 = 'landing_photos';
 
 client
-  .query(`\copy ${table3} FROM '${photosPath}' WITH (FORMAT CSV, HEADER)`)
+  .query(`COPY ${table3} FROM '${photosPath}' WITH (FORMAT CSV, HEADER)`)
   .then(() => {
     console.log('Successfully imported: ', table3);
     client.end();
